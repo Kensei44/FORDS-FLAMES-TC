fae716cca304f85101df5c9b00f4043f07e9617c
alexson <kensei.sin21@gmail.com>
2022-04-08 09:23:13 -0400

    hi

--- FORDS FLAMES TC/pageVC.swift
+++ FORDS FLAMES TC/pageVC.swift
@@ -13,7 +13,41 @@ class pageVC: UIViewController {
         super.viewDidLoad()
 
     }
-    
-
-
+//
+//    @IBAction func TeamReach(_ sender: UIButton)
+//    {
+//
+//           var  = SFSafariViewController(url: URL(string: "http://teamreach.com")!)
+//
+//
+//            }
+//
+//
+//
+//               addViewControllerAsChildViewController()
+//           }
+//
+//            //firstVCIdentifier
+//
+//            func addViewControllerAsChildViewController() {
+//                addChild(safariVC)
+//                self.view.addSubview(safariVC.view)
+//                safariVC.didMove(toParent: self)
+//           setUpConstraints()
+//            }
+//
+//            func setUpConstraints() {
+//                self.safariVC.view.translatesAutoresizingMaskIntoConstraints = false
+//                self.safariVC.view.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 1).isActive = true
+//                self.safariVC.view.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -1).isActive = true
+//                self.safariVC.view.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 1).isActive = true
+//                self.safariVC.view.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -1).isActive = true
+//            }
+//
+//
+//   }
+//    }
+//
+//
+//}
 }
